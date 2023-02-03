b1b98b7f4e8437c84d3a18761386b65ef5eb305c
Maryhei <makemak123@gmail.com>
2023-02-03 23:44:50 +0000

    Added fetch data code

--- technicaltest/src/App.js
+++ technicaltest/src/App.js
@@ -1,11 +1,32 @@
 import './App.css';
 import PieChart from './Components/PieChart';
+import { useEffect, useState } from 'react';
 import React from 'react';
 
 function App() {
+  const [chartData, setChartData] = useState({})
+  const [Data, setData] = useState({})
+  useEffect(() => {
+    const getdata = async () =>{
+      const fetchdata = await fetch('http://substantiveresearch.pythonanywhere.com')
+      const dataToJson = await fetchdata.json()
+      // console.log(dataToJson)
+      const dataForChart = {}
+      dataToJson.forEach(each => {
+        if (each.name in dataForChart){
+          dataForChart[each.name] += 1
+        }else{
+          dataForChart[each.name] = 0
+        }
+      Promise.all(dataToJson)
+      .then(setChartData(dataForChart))
+      })
+    }
+    getdata()
+    // console.log(chartData)
+  })
 
 
-  
   return (
     <div className="App">
 
